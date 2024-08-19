3b7047c14ba0d944d9d5c0b60d148ea99ac03574
Max <max.oblaender@uni-jena.de>
2024-08-19 18:09:16 +0200

    modell Änderungen für dummy variablen

--- glm_rna_seq.R
+++ glm_rna_seq.R
@@ -9,17 +9,20 @@ arab_data$treatment = as.factor(arab_data$treatment)
 ### MODEL ###
 
 model = glm.nb(gene1 ~ treatment + time, data = arab_data)
+#Ich bin mir nicht mehr sicher mit dem treatment und den Dummyvariablen?
 
 summary(model)
 
 results = list()
 
 # Schleife über alle Gene um den Behandlungseffekt für jedes Gen zu schätzen
-for (gene in colnames(data)[4:ncol(data)]) { # erste drei Zeilen sind nur treatment und zeit
-    
-    formula = as.formula(paste(gene, "~ treatment + time")) # as.formula kann den string als formel auszuführen
+# erste drei Zeilen sind nur treatment und zeit
+for (gene in colnames(data)[4:ncol(data)]) {
 
-    model = glm.nb(formula, data = arab_data) # ich weiß nicht mehr genau, wie man die glms richtig hier aufschreibt in R.
+    # as.formula kann den string als Formel auszuführen
+    formula = as.formula(paste(gene, "~ treatment + time"))
+
+    model = glm.nb(formula, data = arab_data) # ich weiß nicht mehr genau, wie man die glms richtig hier aufschreibt in R. # nolint: line_length_linter.
     # @Johanna wollen wir die Uebungen auch einfach in Git hochladen?
     results[[gene]] = summary(model)
 }
