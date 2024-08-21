451eddf870d3fadf90233ae36f290ed0c90341d6
Max <max.oblaender@uni-jena.de>
2024-08-21 15:02:49 +0200

    results save list of models

--- glm_rna_seq.R
+++ glm_rna_seq.R
@@ -19,24 +19,24 @@ results = list()
 
 # Schleife über alle Gene um den Behandlungseffekt für jedes Gen zu schätzen
 # erste drei Zeilen sind nur treatment und zeit
-for (gene in arab_data[,4:ncol(arab_data)]) {
+for (gene in arab_data[, 4:ncol(arab_data)]) {
 
     #as.formula kann den string als Formel auszuführen
     formula = as.formula(paste(gene, "~ as.factor(treatment) + time"))
 
     model = glm.nb(formula, data = arab_data) # ich weiß nicht mehr genau, wie man die glms richtig hier aufschreibt in R. # nolint: line_length_linter.
     # @Johanna wollen wir die Uebungen auch einfach in Git hochladen?
-    results[[gene]] = summary(model)
+    results[[gene]] = model
     #hier ist die Frage ob wir die summary oder das modell selber speichern möchten
 
-# Extrahiere den p-Wert für die Behandlungsvariable
-    treatment_p_value <- coef(model_summary)["treatment2", "Pr(>|z|)"] # p-Wert für die zweite Treatment-Stufe (Behandlung)
+    # Extrahiere den p-Wert für die Behandlungsvariable
+    #treatment_p_value <- coef(model_summary)["treatment2", "Pr(>|z|)"] #TODO hier müssen wir uns überlegen welcher wert significant sein  muss!
     
     # Wenn der p-Wert kleiner als 0,05 ist, als signifikant betrachten
-    if (!is.na(treatment_p_value) && treatment_p_value < 0.05) {
-        significant_genes[[gene_name]] <- treatment_p_value
-    }
+    #if (!is.na(treatment_p_value) && treatment_p_value < 0.05) {
+    #    significant_genes[[gene_name]] <- treatment_p_value
+    #}
 }
 
-save(results, file="complete_model.RData")
+save(results, file="model.RData")
 print(results$Gen1)
