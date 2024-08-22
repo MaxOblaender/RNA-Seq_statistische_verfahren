a1685f93dd3124912ee40e3cd6399205aa2dfe1c
hellforsj <101188865+hellforsj@users.noreply.github.com>
2024-08-22 18:19:58 +0200

    structure/thoughts for simulation

--- simulation.R
+++ simulation.R
@@ -1,17 +1,36 @@
 ### Signifikansüberprüfung: siehe Übung 11_b (Likelihood-Quotienten-Test?)
 
-arab_model=load("model.RData")
+require(MASS)
 
-new_sim_arab_data=""
-i=0
+# Einlesen von den Modelldaten
+model_list=list()
+files = list.files(path="models", full.names=TRUE)
+predict_list=list()
 
-for (model in arab_model){
-    for (i){
-        #Simulieren anhand der Annahme für das Treatment
-        predict(model, newdata=data.frame(treatment="mock"))
-        predict(model, newdata=data.frame(treatment="hcc"))
-    }
-    #Durchschnitt der 1000 Simulationen
-    
-    
-}
+
+### Ausprobieren der Simulierung
+
+model=readRDS(files[1])
+
+# für die Simulierung müssen zufällige Zahlen anhand der neg. Bin. Verteilung und des Modells generiert werden
+# hierfür hab ich noch keine richtige Lösung gefunden
+# in der Übung haben wir das nur für die Poisson-Verteilung gemacht, wo der Syntax anders ist -> geht hier nicht
+test=rnegbin(1000, lambda=predict(model, newdata=data.frame(treatment="mock",time=c(1,2,3))))
+
+print(test)
+
+
+# Modelle können nicht mit ihrer Funktion so einfach in einer Liste gespeichert werden
+# deswegen müssen während des Einlesens alle nötigen Infos generiert werden
+#i=0
+#for (file in files){
+#    i=i+1
+#    model=readRDS(file)
+#    model_list[[i]] = model
+#    predict_list[[i]]=predict(model)
+
+#    for (i in 1:1000){
+
+#    }
+#}
+
