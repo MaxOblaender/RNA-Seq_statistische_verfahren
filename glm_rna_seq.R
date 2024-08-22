6119226d952f9cb14f8fc46b4017c30dd80dcca3
johanna <_!log#456>
2024-08-22 14:37:59 +0200

    clean up and fixes

--- glm_rna_seq.R
+++ glm_rna_seq.R
@@ -1,41 +1,23 @@
+# Programm erstellt Modelle für alle Gene
+
 require(MASS)
 
 ### READ IN ###
 
 arab_data = read.csv("arab.csv")
 
-### MODEL ### Zum testen
-
-gene1 = arab_data[,4]
-model = glm.nb(gene1 ~ as.factor(treatment) + time, data = arab_data) # hier können wir uns noch eine link funktion überlegen
-# mit link = log z.B. 
-#Ich bin mir nicht mehr sicher mit dem treatment und den Dummyvariablen?
-
-print(summary(model))
-
-save(model, file = "test_model.RData")
-
+# Initialisierung der Ergebnisliste
 results = list()
 
 # Schleife über alle Gene um den Behandlungseffekt für jedes Gen zu schätzen
 # erste drei Zeilen sind nur treatment und zeit
+
 for (gene in arab_data[, 4:ncol(arab_data)]) {
 
-    #as.formula kann den string als Formel auszuführen
-    formula = as.formula(paste(gene, "~ as.factor(treatment) + time"))
-
-    model = glm.nb(formula, data = arab_data) # ich weiß nicht mehr genau, wie man die glms richtig hier aufschreibt in R. # nolint: line_length_linter.
-    # @Johanna wollen wir die Uebungen auch einfach in Git hochladen?
-    results[[gene]] = model
-    #hier ist die Frage ob wir die summary oder das modell selber speichern möchten
-
-    # Extrahiere den p-Wert für die Behandlungsvariable
-    #treatment_p_value <- coef(model_summary)["treatment2", "Pr(>|z|)"] #TODO hier müssen wir uns überlegen welcher wert significant sein  muss!
-    
-    # Wenn der p-Wert kleiner als 0,05 ist, als signifikant betrachten
-    #if (!is.na(treatment_p_value) && treatment_p_value < 0.05) {
-    #    significant_genes[[gene_name]] <- treatment_p_value
-    #}
+    # Modell wird für jedes Gen berechnet und in die Liste results hinzugefügt
+    # TODO: link-Funktion
+    model = glm.nb(gene ~ as.factor(treatment)+time, data = arab_data)
+    results = append(results, model)
 }
 
 save(results, file="model.RData")
