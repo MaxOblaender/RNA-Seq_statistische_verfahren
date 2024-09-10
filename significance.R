6584cd36fcd26cdec1c7afabb16a7a3f50b41a7a
Max <max.oblaender@uni-jena.de>
2024-09-10 12:02:50 +0200

    changed p[4] TODO check if proper coefficient

--- significance.R
+++ significance.R
@@ -15,8 +15,9 @@ for (file in files) {
     # zweiter Wert von p stellt p-Wert des treatments dar
     # ist dieses nicht signifikant, ist dieser unter 0.05
     p=summary(model)$coefficients[,4]
+    print(p4)
     signif=TRUE
-    if (p[2]<0.05){
+    if (p[4]<=0.05){
         signif=FALSE
     }
     name=strsplit(file, split="/")[[1]][2]
@@ -25,5 +26,7 @@ for (file in files) {
     
 }
 
-write.csv(p_values, "p_values_for_genes.csv")
+#write.csv(p_values, "p_values_for_genes.csv")
 
+count_significance <- sum(p_values$significant == TRUE)
+count_non_significant = sum(p_values$significant == FALSE)
