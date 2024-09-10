4b9cf3364356a91315ad316781542d930a5f1cca
Max <max.oblaender@uni-jena.de>
2024-09-10 12:08:00 +0200

    changed link fuction to identity -> iterationlimit reached

--- glm_rna_seq.R
+++ glm_rna_seq.R
@@ -1,24 +1,25 @@
-# Programm erstellt Modelle für alle Gene
+    # Programm erstellt Modelle für alle Gene
 
-require(MASS)
+    require(MASS)
 
-### READ IN ###
+    ### READ IN ###
 
-arab_data = read.csv("arab.csv")
+    arab_data = read.csv("arab.csv")
 
-# Namen der Gene
-names=colnames(arab_data)
+    # Namen der Gene
+    names=colnames(arab_data)
 
 
-# Schleife über alle Gene um den Behandlungseffekt für jedes Gen zu schätzen
-# erste drei Zeilen sind nur treatment und zeit
-i=3
-for (gene in arab_data[, 4:ncol(arab_data)]) {
-    i=i+1
-    # Modell wird für jedes Gen berechnet und einzeln gespeichert im Ordner "models"
-    # TODO: link-Funktion
-    model = glm.nb(gene ~ as.factor(time) + as.factor(treatment), data = arab_data)
-    #saveRDS(model, file=paste("models/",names[i],".RData"))
-    print(summary(model))
-}
+    # Schleife über alle Gene um den Behandlungseffekt für jedes Gen zu schätzen
+    # erste drei Zeilen sind nur treatment und zeit
+    i=3
+    for (gene in arab_data[, 4:ncol(arab_data)]) {
+        i=i+1
+        # Modell wird für jedes Gen berechnet und einzeln gespeichert im Ordner "models"
+        # TODO: link-Funktion -> iteration limit reached
+        model = glm.nb(gene ~ as.factor(time) + as.factor(treatment), data = arab_data, link = "identity")
+        #saveRDS(model, file=paste("models/",names[i],".RData"))
+        print(summary(model))
+        print(i)
+    }
 
